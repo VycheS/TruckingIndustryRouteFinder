9694e0b01886e605e83a570952d0b3b2e0ed8f5f
vyches <lieber_freund@mail.ru>
2021-01-19 01:52:11 +0900

    из-за ошибки переименование user в client

--- postgresql/sql/create_tables.sql
+++ postgresql/sql/create_tables.sql
@@ -1,67 +1,42 @@
--- МОДУЛИ
--- подключаем модуль в котором криптографические функции и генерация uuid
-CREATE EXTENSION IF NOT EXISTS "pgcrypto";
+                                 List of databases
+   Name    |  Owner   | Encoding |  Collate   |   Ctype    |   Access privileges   
+-----------+----------+----------+------------+------------+-----------------------
+ postgres  | postgres | UTF8     | en_US.utf8 | en_US.utf8 | 
+ template0 | postgres | UTF8     | en_US.utf8 | en_US.utf8 | =c/postgres          +
+           |          |          |            |            | postgres=CTc/postgres
+ template1 | postgres | UTF8     | en_US.utf8 | en_US.utf8 | =c/postgres          +
+           |          |          |            |            | postgres=CTc/postgres
+ vkr       | postgres | UTF8     | en_US.utf8 | en_US.utf8 | 
+(4 rows)
 
--- ПОЛЬЗОВАТЕЛЬСКИЕ ТИПЫ ДАННЫХ
--- координата(coord)
-CREATE TYPE coord AS (
-    -- широта
-    latitude double precision,
-    -- долгота
-    longitude double precision
-);
--- роль или права доступа(role_type)
-CREATE TYPE role_type AS ENUM (
-    'admin',
-    'user',
-    'driver'
-);
--- тип слоя(layer_type)
-CREATE TYPE layer_type AS ENUM (
-    'point',
-    'line'
-);
+                                 List of databases
+   Name    |  Owner   | Encoding |  Collate   |   Ctype    |   Access privileges   
+-----------+----------+----------+------------+------------+-----------------------
+ postgres  | postgres | UTF8     | en_US.utf8 | en_US.utf8 | 
+ template0 | postgres | UTF8     | en_US.utf8 | en_US.utf8 | =c/postgres          +
+           |          |          |            |            | postgres=CTc/postgres
+ template1 | postgres | UTF8     | en_US.utf8 | en_US.utf8 | =c/postgres          +
+           |          |          |            |            | postgres=CTc/postgres
+ vkr       | postgres | UTF8     | en_US.utf8 | en_US.utf8 | 
+(4 rows)
 
--- СОЗДАЁМ ТАБЛИЦЫ
--- пользователь(user)
-CREATE TABLE user (
-    id serial PRIMARY KEY,
-    surname varchar(25) NOT NULL, -- фамилия
-    name varchar(25) NOT NULL,  -- имя
-    patronymic varchar(25) NOT NULL,  -- отчество
-    email varchar(50) NOT NULL,
-    password varchar NOT NULL,
-    numberphone varchar(25) NOT NULL,
-    role role_type NOT NULL,
-    addjson jsonb,
-    -- проверка на уникальность
-    UNIQUE (email, numberphone)
-);
--- группы слоёв
-CREATE TABLE layer_group (
-    id serial PRIMARY KEY,
-    name varchar(25) NOT NULL
-);
--- ассоциативная таблица для создания многие ко многим между layer_group и user
-CREATE TABLE user_layer_group (
-    user_id integer REFERENCES user(id),
-    layer_group_id integer REFERENCES layer_group(id)
-);
--- слой
-CREATE TABLE layer (
-    id uuid PRIMARY KEY DEFAULT gen_random_uuid(), -- генерируем по умолчанию uuid
-    layer_group_id integer REFERENCES layer_group(id),
-    name varchar(25) NOT NULL,
-    description text NOT NULL,
-    addjson jsonb
-);
--- гео объект
-CREATE TABLE geo_object (
-    id serial PRIMARY KEY,
-    layer_id uuid REFERENCES layer(id),
-    name varchar(25) NOT NULL,
-    -- //TODO сделать проверку чтобы были заполнены оба поля
-    coordinate coord array NOT NULL, -- //TODO сделать проверку для point что можно было ложить только один элемент
-    description text NOT NULL,
-    addjson jsonb
-);
+                                   List of roles
+ Role name |                         Attributes                         | Member of 
+-----------+------------------------------------------------------------+-----------
+ postgres  | Superuser, Create role, Create DB, Replication, Bypass RLS | {}
+
+           List of roles
+ Role name | Attributes | Member of 
+-----------+------------+-----------
+
+                                 List of databases
+   Name    |  Owner   | Encoding |  Collate   |   Ctype    |   Access privileges   
+-----------+----------+----------+------------+------------+-----------------------
+ postgres  | postgres | UTF8     | en_US.utf8 | en_US.utf8 | 
+ template0 | postgres | UTF8     | en_US.utf8 | en_US.utf8 | =c/postgres          +
+           |          |          |            |            | postgres=CTc/postgres
+ template1 | postgres | UTF8     | en_US.utf8 | en_US.utf8 | =c/postgres          +
+           |          |          |            |            | postgres=CTc/postgres
+ vkr       | postgres | UTF8     | en_US.utf8 | en_US.utf8 | 
+(4 rows)
+
