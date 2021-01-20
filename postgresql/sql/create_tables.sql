71f4019733d5983ce3d47469d206596e31c5bb23
vyches <lieber_freund@mail.ru>
2021-01-21 01:31:23 +0900

    добавление домена для фио и для номера телефона

--- postgresql/sql/create_tables.sql
+++ postgresql/sql/create_tables.sql
@@ -23,24 +23,36 @@ CREATE TYPE coord AS (
     longitude double precision
 );
 -- домен поверх coord с проверкой на отсутствие NULL
-CREATE DOMAIN domain_coord AS coord CHECK (
+CREATE DOMAIN domain_coord AS coord
+CHECK (
     ((value).latitude IS NOT NULL) AND ((value).longitude IS NOT NULL)
 );
 -- домен поверх citext для почты
-CREATE DOMAIN email_type AS citext CHECK (
+CREATE DOMAIN email_type AS citext
+CHECK (
     value ~ '^[a-zA-Z0-9.!#$%&''*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$'
+);
+-- домен поверх text для мобильного телефона
+CREATE DOMAIN phone_type AS text
+CHECK (
+    value ~ '^[+]?[0-9]{10,13}$'
+);
+-- домен поверх citext для фамилии имени и отчества
+CREATE DOMAIN name_type AS citext
+CHECK (
+    char_length(value) <= 50
 );
 
 -- СОЗДАЁМ ТАБЛИЦЫ
 -- пользователь(client)
 CREATE TABLE client (
     id serial PRIMARY KEY,
-    surname varchar(25) NOT NULL, -- фамилия
-    name varchar(25) NOT NULL,  -- имя
-    patronymic varchar(25) NOT NULL,  -- отчество
-    password varchar NOT NULL,
+    surname name_type NOT NULL, -- фамилия
+    name name_type NOT NULL,  -- имя
+    patronymic name_type NOT NULL,  -- отчество
+    password varchar(255) NOT NULL,
     email email_type NOT NULL,
-    numberphone varchar(25) NOT NULL, -- //TODO добавить маску(домен) для номера телефона
+    numberphone phone_type NOT NULL,
     role role_type NOT NULL,
     addjson jsonb,
     -- проверка на уникальность
