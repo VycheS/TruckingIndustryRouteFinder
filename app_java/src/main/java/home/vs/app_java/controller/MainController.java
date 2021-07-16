a1de28f8d324f5168d061ff0068824a8b7fba57c
VycheS <lieber_freund@mail.ru>
2021-07-16 23:42:33 +0900

    добавление шаблонов и переписывание на обычный контроллер

--- app_java/src/main/java/home/vs/app_java/controller/MainController.java
+++ app_java/src/main/java/home/vs/app_java/controller/MainController.java
@@ -1,15 +1,17 @@
 package home.vs.app_java.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class MainController {
 
 	@GetMapping("/")
-	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
-		return String.format("Hello %s!", name);
+	public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
+		model.addAttribute("name", name);
+		return "main";
 	}
 
 }
