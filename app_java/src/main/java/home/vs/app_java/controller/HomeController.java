e53ae269f7fc4605b1416bbd389c220039318783
VycheS <lieber_freund@mail.ru>
2021-07-23 12:52:03 +0900

    мелкие исправления в названиях

--- app_java/src/main/java/home/vs/app_java/controller/HomeController.java
+++ app_java/src/main/java/home/vs/app_java/controller/HomeController.java
@@ -6,12 +6,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
-public class MainController {
+public class HomeController {
 
-	@GetMapping("/main")
+	@GetMapping("/home")
 	public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
 		model.addAttribute("name", name);
-		return "main";
+		return "home";
 	}
 
 }
