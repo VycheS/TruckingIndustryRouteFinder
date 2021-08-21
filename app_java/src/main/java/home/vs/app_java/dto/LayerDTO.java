dfec522650473f613dd1a6bfe10944ac7fde7f77
VycheS <lieber_freund@mail.ru>
2021-08-21 23:32:34 +0900

    создание dto layer

---
+++ app_java/src/main/java/home/vs/app_java/dto/LayerDTO.java
@@ -0,0 +1,54 @@
+package home.vs.app_java.dto;
+
+import java.io.Serializable;
+import java.util.UUID;
+
+public class LayerDTO implements Serializable {
+    private static final long serialVersionUID = 1L;
+    
+    private UUID id;
+    private int layerGroupId;
+    private String typeObj;
+    private String name;
+    private String description;
+    private String json;
+
+    public UUID getId() {
+        return id;
+    }
+    public void setId(UUID id) {
+        this.id = id;
+    }
+    public int getLayerGroupId() {
+        return layerGroupId;
+    }
+    public void setLayerGroupId(int layerGroupId) {
+        this.layerGroupId = layerGroupId;
+    }
+    public String getTypeObj() {
+        return typeObj;
+    }
+    public void setTypeObj(String typeObj) {
+        this.typeObj = typeObj;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getDescription() {
+        return description;
+    }
+    public void setDescription(String description) {
+        this.description = description;
+    }
+    public String getJson() {
+        return json;
+    }
+    public void setJson(String json) {
+        this.json = json;
+    }
+
+    
+}
