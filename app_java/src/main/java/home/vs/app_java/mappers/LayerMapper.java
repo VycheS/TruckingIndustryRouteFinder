919cdeadcb31534295407a0e2907d7f2c45083dd
VycheS <lieber_freund@mail.ru>
2021-08-21 23:34:28 +0900

    создание layer mapper

---
+++ app_java/src/main/java/home/vs/app_java/mappers/LayerMapper.java
@@ -0,0 +1,28 @@
+package home.vs.app_java.mappers;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.UUID;
+
+import org.springframework.jdbc.core.RowMapper;
+
+import home.vs.app_java.dto.LayerDTO;
+
+public class LayerMapper implements RowMapper<LayerDTO> {
+    @Override
+    public LayerDTO mapRow(ResultSet rs, int i) throws SQLException {
+        LayerDTO layer = new LayerDTO();
+
+        String strUUID = rs.getString("id");
+        UUID uuid = UUID.fromString(strUUID);
+
+        layer.setId(uuid);
+        layer.setLayerGroupId(rs.getInt("layer_group_id"));
+        layer.setTypeObj(rs.getString("type_obj"));
+        layer.setName(rs.getString("name"));
+        layer.setDescription(rs.getString("description"));
+        layer.setJson(rs.getString("addjson"));
+
+        return layer;
+    }
+}
