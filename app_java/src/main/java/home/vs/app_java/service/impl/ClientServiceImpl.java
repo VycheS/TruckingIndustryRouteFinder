3af8796eadc13045bf3ce1c92a700a796295e3e3
VycheS <lieber_freund@mail.ru>
2021-12-08 15:56:00 +0900

    изменение клиент сервиса для работы через dao

--- app_java/src/main/java/home/vs/app_java/service/impl/ClientServiceImpl.java
+++ app_java/src/main/java/home/vs/app_java/service/impl/ClientServiceImpl.java
@@ -1,40 +1,40 @@
 package home.vs.app_java.service.impl;
 
-import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import home.vs.app_java.dao.FakeDB;
+import home.vs.app_java.dao.ClientDAO;
 import home.vs.app_java.dto.ClientDTO;
 import home.vs.app_java.service.ClientService;
 
 @Service
 public class ClientServiceImpl implements ClientService {
 
+    @Autowired
+    private ClientDAO clientDAO;
+
     @Override
     public boolean create(ClientDTO client) {
-        final int clientId = FakeDB.Client.ID_HOLDER.incrementAndGet();
-        client.setId(clientId);
-        FakeDB.Client.MAP.put(clientId, client);
+        this.clientDAO.save(client);
         return true;
     }
 
     @Override
     public List<ClientDTO> readAll() {
-        return new ArrayList<>(FakeDB.Client.MAP.values());
+        return this.clientDAO.getAll();
     }
 
     @Override
     public ClientDTO read(int id) {
-        return FakeDB.Client.MAP.get(id);
+        return this.clientDAO.get(id);
     }
 
     @Override
     public boolean update(ClientDTO client, int id) {
-        if (FakeDB.Client.MAP.containsKey(id)) {
-            client.setId(id);
-            FakeDB.Client.MAP.put(id, client);
+        if (this.clientDAO.get(id) != null) { //TODO сделать нормальную обработку ошибок
+            this.clientDAO.update(id, client);
             return true;
         }
         return false;
@@ -42,7 +42,11 @@ public class ClientServiceImpl implements ClientService {
 
     @Override
     public boolean delete(int id) {
-        return FakeDB.Client.MAP.remove(id) != null;
+        if (this.clientDAO.get(id) != null) { //TODO сделать нормальную обработку ошибок
+            this.clientDAO.delete(id);
+            return true;
+        }
+        return false;
     }
     
 }
