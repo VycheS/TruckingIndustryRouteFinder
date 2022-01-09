b1f11e7c50c2640dc2bf640f4f27e7378b860497
VycheS <lieber_freund@mail.ru>
2022-01-10 01:55:20 +0900

    правка названия базы

--- postgresql/sql/recreate_db.sql
+++ postgresql/sql/recreate_db.sql
@@ -1,4 +1,4 @@
 -- удаляем бд 'vkr' если существует
-DROP DATABASE IF EXISTS vkr;
+DROP DATABASE IF EXISTS map_jdbctemplate;
 -- создаём заного бд 'vkr'
-CREATE DATABASE vkr;
+CREATE DATABASE map_jdbctemplate;
