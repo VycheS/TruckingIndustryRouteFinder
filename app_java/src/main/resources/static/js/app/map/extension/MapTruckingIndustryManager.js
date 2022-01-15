d825180fa0ef66b088c79989f3d4cd6e1f895dd3
VycheS <lieber_freund@mail.ru>
2022-01-15 12:04:07 +0900

    исправление багов, мелкие правки

--- app_java/src/main/resources/static/js/app/map/extension/MapTruckingIndustryManager.js
+++ app_java/src/main/resources/static/js/app/map/extension/MapTruckingIndustryManager.js
@@ -7,24 +7,12 @@ class MapTruckingIndustryManager extends MapLayerGeoObjectManager {
         this._menuTruck = document.querySelector('#menuTruck');
         //контекстное меню пункта доставки товара
         this._menuDeliveryPoint = document.querySelector('#menuDeliveryPoint');
-        //имя типа геообъекта в балуне на русском
-        this._typeNameInnerBalloon;
-        //параметры для геообъекта грузоперевозок
-        this._paramsTruckingIndustry;
 
     }
 
     _addProperties(obj, convertedDto) {
-        //применяем
-        this._paramsTruckingIndustry = convertedDto.params.trucking_industry;
-        //в зависимости от типа выбираем нужное окно
-        if (this._paramsTruckingIndustry.type == 'deliveryPoint') {
-            this._typeNameInnerBalloon = "Пункт доставки";
-        } else if (this._paramsTruckingIndustry.type == 'truck') {
-            this._typeNameInnerBalloon = "Грузоперевозчик";
-        } else if (this._paramsTruckingIndustry.type == 'goods') {
-            this._typeNameInnerBalloon = "Товар/Груз";
-        } else throw Error(`Такого типа геообъектов:\"${this._paramsTruckingIndustry}\" для грузоперевозок, не существует`);
+        //добавляем геообъекту дополнительное грузоперевозочное св-во через который в последующем будем работать
+        obj.trucking_industry = convertedDto.params.trucking_industry;
         let nameGeoObj; //имя геообъекта
         //заголовком будет индекс геообъекта в его слое.  
         if ([null, ''].includes(convertedDto.name)) {
@@ -38,7 +26,14 @@ class MapTruckingIndustryManager extends MapLayerGeoObjectManager {
         let hintDescription; //описание подсказка
         // меняем подсказку объекта
         if ([null, ''].includes(convertedDto.description)) {
-            hintDescription = this._typeNameInnerBalloon;
+            //в зависимости от типа выбираем нужное окно
+            if (obj.trucking_industry.type == 'deliveryPoint') {
+                hintDescription = "Пункт доставки";
+            } else if (obj.trucking_industry.type == 'truck') {
+                hintDescription = "Грузоперевозчик";
+            } else if (obj.trucking_industry.type == 'goods') {
+                hintDescription = "Товар/Груз";
+            } else throw Error(`Такого типа геообъектов:\"${obj.trucking_industry.type}\" для грузоперевозок, не существует`);
             obj.properties.set('hintContent', hintDescription);
             this._layerStorage.get(obj.layerName).arrGeoObjects[obj.indexId].description = hintDescription; //в том числе и имя в geoObjDTO
         } else {
@@ -60,38 +55,38 @@ class MapTruckingIndustryManager extends MapLayerGeoObjectManager {
             //тип открывающегося меню который ложиться в URL
             let typeMenu;
             //в зависимости от типа выбираем нужное окно
-            if (this._paramsTruckingIndustry.type == 'deliveryPoint') {
+            if (obj.trucking_industry.type == 'deliveryPoint') {
                 typeMenu = '#menuDeliveryPoint';
                 menu = this._menuDeliveryPoint;
-            } else if (this._paramsTruckingIndustry.type == 'truck') {
+            } else if (obj.trucking_industry.type == 'truck') {
                 typeMenu = '#menuTruck';
                 menu = this._menuTruck;
-            } else if (this._paramsTruckingIndustry.type == 'goods') {
+            } else if (obj.trucking_industry.type == 'goods') {
                 typeMenu = '#menuGoods';
                 menu = this._menuGoods;
-            } else throw Error(`Такого типа геообъектов:\"${this._paramsTruckingIndustry}\" для грузоперевозок, не существует`);
+            } else throw Error(`Такого типа геообъектов:\"${obj.trucking_industry.type}\" для грузоперевозок, не существует`);
             //меняем координаты окна перед появлением
             menu.style.left = e.get('pagePixels')[0] + 'px';
             menu.style.top = e.get('pagePixels')[1] + 'px';
             //открываем окно "настройки геообъекта"
             location.hash = typeMenu;
             //добавляем обработку события для окна добавления опции к объекту
-            this._menu.addEventListener('submit',  e => {
+            menu.addEventListener('submit',  e => {
                 //отключаем перезагрузку страницы при нажатии на submit
                 e.preventDefault();
-                if (this._paramsTruckingIndustry.type == 'truck') {
+                if (obj.trucking_industry.type == 'truck') {
                     //получаем грузоподъёмность грузоперевозчика
-                    this._paramsTruckingIndustry.carrying = this._menu.carrying.value;
-                } else if (this._paramsTruckingIndustry.type == 'goods') {
+                    obj.trucking_industry.carrying = this._menu.carrying.value;
+                } else if (obj.trucking_industry.type == 'goods') {
                     //получаем вес груза
-                    this._paramsTruckingIndustry.weight = this._menu.weight.value;
+                    obj.trucking_industry.weight = this._menu.weight.value;
                     //получаем имя пункта доставки
-                    this._paramsTruckingIndustry.deliveryPoint = this._menu.deliveryPoint.value;
+                    obj.trucking_industry.deliveryPoint = this._menu.deliveryPoint.value;
                 }
 
                 let objJson = JSON.parse(this._layerStorage.get(obj.layerName).arrGeoObjects[obj.indexId].strJson);
 
-                objJson.trucking_industry = this._paramsTruckingIndustry;
+                objJson.trucking_industry = obj.trucking_industry;
 
                 this._layerStorage.get(obj.layerName).arrGeoObjects[obj.indexId].strJson = JSON.stringify(objJson);
                 //очищаем все поля ввода после применить
