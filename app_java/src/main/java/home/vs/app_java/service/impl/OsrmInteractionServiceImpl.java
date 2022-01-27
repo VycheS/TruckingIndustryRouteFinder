ce0551eae026a3eb6cb0edccdb1afdf95e799c71
VycheS <lieber_freund@mail.ru>
2022-01-28 02:04:33 +0900

    исправления

--- app_java/src/main/java/home/vs/app_java/service/impl/OsrmInteractionServiceImpl.java
+++ app_java/src/main/java/home/vs/app_java/service/impl/OsrmInteractionServiceImpl.java
@@ -1,9 +1,7 @@
 package home.vs.app_java.service.impl;
 
-import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 
